89d13b8fe8ddef4898cdbd97fa67b446995ff314
kunj2807 <kunjgadhesariya@gmail.com>
2022-07-07 19:09:56 +0530

    submission files

---
+++ 200103101006-JS/external.js
@@ -0,0 +1,11 @@
+function alertexternal(){
+    alert("You are in external javascript");
+}
+function confirmexternal(){
+confirm("Are you sure..??")
+}
+function promptexternal(){
+    let first=prompt('Enter your first name');
+    let second=prompt('Enter your second name');
+    alert(first+" "+second);
+}
